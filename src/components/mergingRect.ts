4d4ebe5ef98cb068a87e4a7e3787293d1eb65941
huangluye <luyeade@163.com>
2019-10-25 11:04:51 +0800

    update done

--- src/components/mergingRect.ts
+++ src/components/mergingRect.ts
@@ -1,33 +1,38 @@
-import { Coordinate } from '../interface/components'
+import { Coordinate } from '../types/position'
 export default class {
     el: HTMLDivElement
     initialPos: Coordinate
+    offsetTop: number
+    offsetLeft: number
     constructor(x: number, y: number) {
         this.el = document.createElement('div')
+        const parent = window.container || {}
+        this.offsetTop = parent.offsetTop
+        this.offsetLeft = parent.offsetLeft
+        console.log(parent, this.offsetTop, this.offsetLeft)
         this.el.classList.add('merging-rect')
         this.initialPos = { x, y }
         this.render()
-        this._locate()
+        this._locate(this.initialPos)
     }
-    _locate() {
-        this.el.style.top = this.initialPos.y - this.el.offsetTop + 'px'
-        this.el.style.left = this.initialPos.x - this.el.offsetLeft + 'px'
-        console.log(this.el.style)
+    _locate(c: Coordinate) {
+        this.el.style.top = c.y - this.offsetTop + 'px'
+        this.el.style.left = c.x - this.offsetLeft + 'px'
     }
     render(): void {
         window.container.appendChild(this.el)
     }
 
-    update(c: Coordinate): void
-    update(c: Coordinate, isSize: boolean = false): void {
-        console.log(c, this.initialPos)
-        if (!isSize) {
-            c.x -= this.initialPos.x
-            c.y -= this.initialPos.y
+    // update(c: Coordinate): void
+    update(c: Coordinate): void {
+        let { x, y } = c
+        const rebase = x < this.initialPos.x || y < this.initialPos.y
+        const [w, h] = [Math.abs(c.x - this.initialPos.x), Math.abs(c.y - this.initialPos.y)]
+        if (rebase) {
+            this._locate({ x: Math.min(x, this.initialPos.x), y: Math.min(y, this.initialPos.y) })
         }
-        // console.log(c)
-        this.el.style.width = c.x + 'px'
-        this.el.style.height = c.y + 'px'
+        this.el.style.width = w + 'px'
+        this.el.style.height = h + 'px'
     }
     destroy() {
         this.el.remove()
--- src/types/position.ts
+++ src/types/position.ts
@@ -1,4 +1,4 @@
-export interface Coordinate {
+export type Coordinate = {
     x: number,
     y: number,
     // job?: string, //可选属性，表示不是必须的参数，
